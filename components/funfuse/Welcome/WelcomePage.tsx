3ac71b91f88cf520509b5411bbc6647afd8acf10
sedhha <activity.schoolsh2@gmail.com>
2022-04-15 15:23:53 +0530

    Updated Images

---
+++ components/funfuse/Welcome/WelcomePage.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import classes from './WelcomePage.module.scss';
+
+export default function WelcomePage() {
+  return <div className={classes.Card}></div>;
+}
---
+++ pages/funfuse/index.tsx
@@ -0,0 +1,8 @@
+import WelcomePage from '@/components/funfuse/Welcome/WelcomePage';
+import React from 'react';
+
+type Props = {};
+
+export default function Welcome({}: Props) {
+  return <WelcomePage />;
+}
