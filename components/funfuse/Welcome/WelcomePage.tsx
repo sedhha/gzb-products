3b89ecf661b915f08e84141258d93a37d81f38c8
sedhha <activity.schoolsh2@gmail.com>
2022-04-15 20:00:08 +0530

    Updated Welcome Page Path

--- components/funfuse/Welcome/WelcomePage.tsx
+++ components/funfuse/Welcome/WelcomePage.tsx
@@ -4,13 +4,27 @@ import WhiteLogo from '@/components/funfuse/Logos/WhiteLogo';
 import WhiteButton from '@/components/funfuse/Buttons/WhiteButton/WhiteButton';
 import { FiLogIn } from 'react-icons/fi';
 import { AiOutlinePlus } from 'react-icons/ai';
+import { useRouter } from 'next/router';
 export default function WelcomePage() {
+  const router = useRouter();
   return (
     <div className={classes.Card}>
       <div className='flex flex-row justify-center w-full p-0 m-0 text-center gap-x-4'>
-        <WhiteButton icon={<FiLogIn className={classes.ActionIcon} />} />
         <WhiteButton
-          icon={<AiOutlinePlus className={classes.ActionIconLarge} />}
+          icon={
+            <FiLogIn
+              className={classes.ActionIcon}
+              onClick={() => router.push('/funfuse/login')}
+            />
+          }
+        />
+        <WhiteButton
+          icon={
+            <AiOutlinePlus
+              className={classes.ActionIconLarge}
+              onClick={() => router.push('/funfuse/register')}
+            />
+          }
         />
       </div>
       <div className={classes.LogoHeight}>
