e0643e88598d4f77d3be273dd9b92c94352052b3
sedhha <activity.schoolsh2@gmail.com>
2022-04-24 09:08:08 +0530

    Removed Middleware Route

--- pages/api/test-route/_middleware.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export const middleware = () => {
-  if (process.env.NODE_ENV === 'development') {
-    return NextResponse.json({ msg: 'Undefined' });
-  }
-};
