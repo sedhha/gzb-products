774485ed91125d4a1e2cf12414575ca57045d004
sedhha <activity.schoolsh2@gmail.com>
2022-05-02 12:47:34 +0530

    Auth Signout

--- components/funfuse/VerifyEmail/VerifyEmail.tsx
+++ components/funfuse/VerifyEmail/VerifyEmail.tsx
@@ -77,6 +77,7 @@ export default function LoginPage() {
                 ]
               );
               setSuccess('');
+              Auth.signOut();
               return;
             }
           });
