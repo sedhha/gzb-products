4180b0fc075d63120510eefd6175a3c387f25dd1
sedhha <activity.schoolsh2@gmail.com>
2022-05-07 20:49:50 +0530

    Updated Events Page

--- components/funfuse/VerifiedController/VerifiedPages/Events/Events.tsx
+++ components/funfuse/VerifiedController/VerifiedPages/Events/Events.tsx
@@ -1,5 +1,18 @@
+import IMessage from '@/components/funfuse/IllustrationMessage/IMessage';
 import React from 'react';
 
 export default function HomePage() {
-  return <div>This is Events Route</div>;
+  return (
+    <IMessage
+      src={'/funfuse/events-page.svg'}
+      alt={'No Connections'}
+      JSXMessageComponent={
+        <label className='text-center text-gray-400 font-md font-funfuse'>
+          Great things are on the way. We&apos;re still working on this feature.
+          Here you all can hop in and promote your businesses, talk about new
+          ideas and do epic stuff! Stay tuned and soon its going to be live!
+        </label>
+      }
+    />
+  );
 }
